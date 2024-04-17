357a50fd639245e3ff6d6ae5a0648d70d8cea6fd
Rahul <rajamohanty12345@gmail.com>
2024-04-17 17:46:19 +0530

    theme default set to light

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from "@/components/themes/theme-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <BrowserRouter>
         <AuthContextProvider>
           <UserContextProvider>
